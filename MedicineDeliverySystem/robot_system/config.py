1450770d3ad6285637b3c6f18745c81a94d1a01e
Andres1911 <juan.gonzalezreina@mail.mcgill.ca>
2026-03-24 19:18:50 -0400

    config update

--- MedicineDeliverySystem/robot_system/config.py
+++ MedicineDeliverySystem/robot_system/config.py
@@ -68,8 +68,8 @@ class Config:
         S1_SEGMENT2 = 24.3  # cm to move forward in segment 2
         S1_SEGMENT3 = 16  # cm to move forward in segment 3 (to patient room)
 
-        S2_SEGMENT1 = 15  # cm to move forward in segment 1 (to patient room)
+        S2_SEGMENT1 = 18  # cm to move forward in segment 1 (to patient room)
         S2_SEGMENT2 = 44.1  # cm to move forward in segment 2
 
-        ROOM_SWEEP_LENGTH = 40 # cm to sweep across room for obstacle detection
+        ROOM_SWEEP_LENGTH = 20 # cm to sweep across room for obstacle detection
         SWEEP_DIFF = 5 # cm to move forward/backward during sweep turns
