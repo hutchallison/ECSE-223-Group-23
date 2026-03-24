f955a753894567ebb84b99aa1a93700b8b68e6c4
zaid <zaidkhanfer@gmail.com>
2026-03-24 16:38:13 -0400

    fixed directory issues

--- MedicineDeliverySystem/robot_system/tests/Pathtesting.py
+++ MedicineDeliverySystem/robot_system/tests/Pathtesting.py
@@ -1,58 +1,47 @@
 #!/usr/bin/python3
 
+import sys
+import os
 import time
 import numpy as np
-import threading # <-- 1. Import threading
+import threading
+import pickle
+import simpleaudio as sa
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+if current_dir not in sys.path:
+    sys.path.append(current_dir)
+
 from create_gauss import create_gaussian
 from bhatta_dist import bhatta_distance
 from utils.brick import EV3ColorSensor, wait_ready_sensors, TouchSensor, Motor
-import pickle
 from utils.sound import Sound
-import simpleaudio as sa
 from navigator import Navigator
 
 nav = Navigator()
-
-#color sensor initializer
 color_sensor = EV3ColorSensor(3)
-
-#initializes window size for color sensor data
 WINDOW_SIZE = 500
-
-#name of color calibration file to use
 COLOR_FILE = "final_project.cal"
 
-#creates 2 tone variables for the flute to play
 tone1 = Sound(duration=1.0, volume=100, pitch="C4")
 tone2 = Sound(duration=1.0, volume=100, pitch="G4")
 
-#dictionary with colors and matching tones
 COLOR_TO_SOUND = {
-#	"red": tone1,
 	"green": tone2,
 }
 
-#imports color calibration file
 try:
 	with open(COLOR_FILE, 'rb') as color_file:
 		known_colors = pickle.load(color_file)
 except Exception as e:
     print("error loading file "+ str(e))
 
-#initializes numpy array which holds the most recent data
 unknown_color_data = np.zeros((3, WINDOW_SIZE))
-
-#initializes sensors
 wait_ready_sensors(True)
 
-
 def detect_color():
-	'''
-	Collects 500 data samples in sliding window creates a Gaussian...
-	'''
 	global unknown_color_data
 
-	#Collects data points and adds the to the sliding window of data
 	for i in range(WINDOW_SIZE//4):
 		time.sleep(0.00001)
 		rgb_values = color_sensor.get_rgb()
@@ -62,10 +51,8 @@ def detect_color():
 			red, green, blue = rgb_values
 			unknown_color_data[:, -1] = [int(red), int(green), int(blue)]
 	
-	#Creates a gaussian with the sliding window data
 	mean1, cov1 = create_gaussian(unknown_color_data)
 
-	#initializes minimum distance and color with minimum distance
 	min_bhatta_dist = None
 	min_bhatta_dist_color = None
 
@@ -77,36 +64,29 @@ def detect_color():
 		if min_bhatta_dist is None:
 			min_bhatta_dist = current_bhatta_dist
 			min_bhatta_dist_color = key
-
 		elif current_bhatta_dist is None:
 			pass
-
 		elif current_bhatta_dist < min_bhatta_dist:
 			min_bhatta_dist = current_bhatta_dist
 			min_bhatta_dist_color = key
 	
 	return min_bhatta_dist_color
 
-# --- 2. THREADING SETUP ---
+
 current_color = None
 color_lock = threading.Lock()
 running = True 
 
 def color_worker():
-    """Background thread function that constantly updates the current color."""
     global current_color, running
     while running:
         detected = detect_color()
-        # Securely update the shared variable
         with color_lock:
             current_color = detected
 
-# Start the background thread
 color_thread = threading.Thread(target=color_worker, daemon=True)
 color_thread.start()
-# --------------------------
 
-#initializes main loop variables
 previous_color = None
 
 nav.move_forward(60)
@@ -115,7 +95,6 @@ try:
 	while True:
 		time.sleep(0.1)
 		
-		# 3. Securely read the latest color from the background thread
 		with color_lock:
 			color = current_color
 			
@@ -131,9 +110,7 @@ try:
 			nav.move_backward(30)
 			break
 		else:
-			# nav.move_forward(3)
 			pass
 
 finally:
-	# Clean up thread on exit
 	running = False
