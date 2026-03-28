26d4a330a386986f972a2f2033d9b2a2b1c4d6ec
Andres1911 <juan.gonzalezreina@mail.mcgill.ca>
2026-03-27 23:10:16 -0400

    ed2

--- MedicineDeliverySystem/robot_system/controller.py
+++ MedicineDeliverySystem/robot_system/controller.py
@@ -121,7 +121,7 @@ class Controller:
     def go_to_room4(self):
         self.nav.turn(-90)
         self.nav.move_backward(Config.Controller.BLACK_LINE_SEGMENT, assessor=self.assessor)
-        self.nav.turn(-90)
+        self.nav.turn(90)
     
     def sweep_room4(self):
         self.sweep_room(Config.Controller.SWEEP_ANGLE, Config.Controller.OBSTACLE_SWEEP_ANGLE)
